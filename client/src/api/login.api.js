a00aa5a8912834fed268669f75591a23602701cf
Shaiv Pandya <51866463+ShaivPandya@users.noreply.github.com>
2022-07-07 18:26:07 -0600

    Finish login system

--- client/src/api/login.api.js
+++ client/src/api/login.api.js
@@ -2,22 +2,21 @@ import axios from 'axios';
 import { loginSuccessfully, loginFailed } from '../store/actions/auth.action';
 import { HOST, LOGIN_URI, USER_TOKEN } from '../constants';
 import { setTokenToLocalStorage } from '../utils';
-// import querystring from 'querystring';
 
 // eslint-disable-next-line import/prefer-default-export
 export const login = userData => dispatch => {
-  console.log(HOST, LOGIN_URI, userData);
+  console.log('userData sent to login: ', userData);
   axios
     .post(`${HOST}${LOGIN_URI}`, userData)
     .then(res => {
+      console.log('res: ', res);
       // Set userToken to Local Storage
-      setTokenToLocalStorage(USER_TOKEN, res.data.access_token).then(() => {
-        console.log(res);
+      setTokenToLocalStorage(USER_TOKEN, res.data.refresh_token).then(() => {
         dispatch(loginSuccessfully(res.data));
       });
     })
     .catch(err => {
-      console.log(`error: ${err}`);
+      console.log('err: ', err.response.data);
       dispatch(loginFailed(err));
     });
 };
